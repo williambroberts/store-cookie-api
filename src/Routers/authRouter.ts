e7b9f96c170347c2421fbb83e4558014eb503b84
= <=>
2023-09-22 08:16:21 +0100

    r

--- src/Routers/authRouter.ts
+++ src/Routers/authRouter.ts
@@ -1,6 +1,6 @@
 import express from "express"
-import { loginVS, valitatorVS } from "../utils/Validators"
-import { loginController, logoutController, statusController } from "../Controllers/authControllers"
+import { loginVS, registerVS, valitatorVS } from "../utils/Validators"
+import { loginController, logoutController, registerController, statusController } from "../Controllers/authControllers"
 import { enableAuthenticate } from "../Middleware/authMiddleware"
 
 const authRouter = express.Router()
@@ -8,7 +8,7 @@ const authRouter = express.Router()
 
 authRouter.post("/login",loginVS,valitatorVS,loginController)
 authRouter.post("/logout",logoutController)
-
+authRouter.post("/regitser",registerVS,valitatorVS,registerController)
 
 // protected test if really auth validate cookie
 authRouter.get("/status",enableAuthenticate,statusController)
