0e889f72951fc721e18e3c86a08ce864918b5497
= <=>
2023-09-22 08:17:57 +0100

    built

--- dist/Routers/authRouter.js
+++ dist/Routers/authRouter.js
@@ -10,6 +10,7 @@ const authMiddleware_1 = require("../Middleware/authMiddleware");
 const authRouter = express_1.default.Router();
 authRouter.post("/login", Validators_1.loginVS, Validators_1.valitatorVS, authControllers_1.loginController);
 authRouter.post("/logout", authControllers_1.logoutController);
+authRouter.post("/regitser", Validators_1.registerVS, Validators_1.valitatorVS, authControllers_1.registerController);
 // protected test if really auth validate cookie
 authRouter.get("/status", authMiddleware_1.enableAuthenticate, authControllers_1.statusController);
 exports.default = authRouter;
